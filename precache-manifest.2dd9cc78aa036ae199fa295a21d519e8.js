62e9ce12e32343ad6a322dc159dd006a87c19d63
giantrotta24 <giantrotta@me.com>
2019-03-19 18:45:51 -0400

    Updates

--- precache-manifest.2dd9cc78aa036ae199fa295a21d519e8.js
+++ precache-manifest.2dd9cc78aa036ae199fa295a21d519e8.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
   },
   {
-    "revision": "cba4e222d327cd2831b6",
-    "url": "/clicky-game/static/js/main.de9f1565.chunk.js"
+    "revision": "3de260a697ea0c83d269",
+    "url": "/clicky-game/static/js/main.9cb632ae.chunk.js"
   },
   {
     "revision": "a145b351ca2c6996d84d",
     "url": "/clicky-game/static/js/2.b739a309.chunk.js"
   },
   {
-    "revision": "cba4e222d327cd2831b6",
+    "revision": "3de260a697ea0c83d269",
     "url": "/clicky-game/static/css/main.07c2a332.chunk.css"
   },
   {
-    "revision": "6e89a1848e59f9a8201e2ca1fe4bffae",
+    "revision": "f7818fe5e37b196edb7b91ebd583d46f",
     "url": "/clicky-game/index.html"
   }
 ];
