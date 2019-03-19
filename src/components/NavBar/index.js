fc341e94233542fe0c3c92bdb3a935be9edd5f7c
giantrotta24 <giantrotta@me.com>
2019-03-19 18:50:19 -0400

    another one

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 export function NavBar(props) {
   return (
     <nav className="navbar navbar-dark bg-dark d-flex justify-content-around">
-      <a className="navbar-brand" href="/">Clicky Game!</a>
+      <a className="navbar-brand" href="/clicky-game/">Clicky Game!</a>
       <span className="navbar-text" id="message">{props.message}</span>
       <span className="navbar-text">Score: {props.score} | Top Score: {props.topScore}</span>
     </nav>
