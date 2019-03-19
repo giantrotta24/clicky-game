84b430ee7cea9609dc9dc045abe5d7186b822447
giantrotta24 <giantrotta@me.com>
2019-03-19 17:01:29 -0400

    Updates

---
+++ precache-manifest.0af9f4ab72e3e558eac1170bed73f490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5eb200277bdd8ab03375ebdefd24e30",
+    "url": "/clicky-game/static/media/rick_and_morty_background_no_4_by_jurokoo-dahrvd7 15-19-47-948.b5eb2002.png"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "150d431ddc6ba0b68989",
+    "url": "/clicky-game/static/js/main.8600eb1e.chunk.js"
+  },
+  {
+    "revision": "a145b351ca2c6996d84d",
+    "url": "/clicky-game/static/js/2.b739a309.chunk.js"
+  },
+  {
+    "revision": "150d431ddc6ba0b68989",
+    "url": "/clicky-game/static/css/main.07c2a332.chunk.css"
+  },
+  {
+    "revision": "290ee9de71c8aac3ceafdb03ae142653",
+    "url": "/clicky-game/index.html"
+  }
+];
